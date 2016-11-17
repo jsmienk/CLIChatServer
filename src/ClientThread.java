891975612d6ea51c93f7bff63a5ffce9cae50ae5
SvenOrdelman <sveno98@live.nl>
2016-11-17 14:01:57 +0100

    Server Set Up

---
+++ src/ClientThread.java
@@ -0,0 +1,19 @@
+import java.net.Socket;
+
+/**
+ * Created by sveno on 17-11-2016.
+ */
+public class ClientThread extends Thread{
+        private Socket socket;
+
+        public ClientThread(Socket socket) {
+            this.socket = socket;
+                System.out.println("Client connected");
+        }
+
+        public void run() {
+            // 1. Wacht op berichten van de client.
+            // 2. Stuur berichten van de clients door naar de andere
+            // clients. (Broadcast)
+        }
+}
---
+++ src/Server.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by sveno on 17-11-2016.
+ */
+public class Server {
+    private static int SERVER_PORT = 1234;
+    private ServerSocket serverSocket;
+
+    public void run(){
+        try {
+            serverSocket = new ServerSocket(SERVER_PORT);
+            System.out.println("Server started on port: " + SERVER_PORT);
+            Socket socket = serverSocket.accept();
+            ClientThread ct = new ClientThread(socket);
+            ct.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String Args[]){
+        new Server().run();
+    }
+}
