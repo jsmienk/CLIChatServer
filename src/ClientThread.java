5e66143c4b5bb215b65a7a2bc130e9331e3c0688
SvenOrdelman <sveno98@live.nl>
2016-11-17 14:23:47 +0100

    Recieving

--- src/ClientThread.java
+++ src/ClientThread.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
 
 /**
@@ -8,21 +6,34 @@ import java.net.Socket;
  */
 public class ClientThread extends Thread {
     private Socket socket;
+    private OutputStream os;
 
     public ClientThread(Socket socket) {
         this.socket = socket;
         System.out.println("Client connected");
+        try {
+            os = socket.getOutputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        PrintWriter writer = new PrintWriter(os);
+        writer.println("Welkom bij de server");
+        writer.flush();
+
+
     }
 
     public void run() {
-        while (true) {
+        while (socket.isConnected()) {
             BufferedReader reader = null;
             try {
                 reader = new BufferedReader(
                         new InputStreamReader(socket.getInputStream()));
                 String clientData = "";
                 clientData = reader.readLine();
-                System.out.println(clientData);
+                if (clientData != "") {
+                    System.out.println(clientData);
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
